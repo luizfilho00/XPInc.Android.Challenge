eba92aa3ab1966cc559385499ec6a2080330a420
Luiz Filho <luiz.filho@xpi.com.br>
2023-06-15 19:28:19 -0400

    Add doc

--- contacts/src/main/java/br/com/xpinc/mobile/contacts/ContactsApiService.kt
+++ contacts/src/main/java/br/com/xpinc/mobile/contacts/ContactsApiService.kt
@@ -8,13 +8,45 @@ import kotlin.random.Random
 import kotlinx.coroutines.delay
 import org.json.JSONArray
 
+/**
+ * Interface that defines the methods to retrieve contacts from the API service.
+ */
 interface ContactsApiService {
+
+    /**
+     * Retrieves all contacts.
+     *
+     * @return The list of contacts.
+     */
     suspend fun getContacts(): List<Contact>
+
+    /**
+     * Retrieves contacts for a specific page.
+     *
+     * @param page The page number.
+     * @return The list of contacts for the specified page.
+     */
     suspend fun getContactsByPage(page: Int): List<Contact>
+
+    /**
+     * Retrieves all contacts as a single.
+     *
+     * @return The single containing the list of contacts.
+     */
     fun getSingleContacts(): Single<List<Contact>>
+
+    /**
+     * Retrieves contacts for a specific page as a single.
+     *
+     * @param page The page number.
+     * @return The single containing the list of contacts for the specified page.
+     */
     fun getSingleContactsByPage(page: Int): Single<List<Contact>>
 }
 
+/**
+ * Implementation of the ContactsApiService interface.
+ */
 class ContactsApiServiceImpl(
     private val resources: Resources,
     private val sleepTime: Long = 800,
@@ -22,12 +54,25 @@ class ContactsApiServiceImpl(
     private val contactLimitSize: Int = 100,
 ) : ContactsApiService {
 
+    private val mapPaging: MutableMap<Int, List<Contact>> = hashMapOf()
+
+    /**
+     * Retrieves all contacts.
+     *
+     * @return The list of contacts.
+     */
     override suspend fun getContacts(): List<Contact> {
         delay(sleepTime)
         simulateError()
         return loadFromFile(paging = false)
     }
 
+    /**
+     * Retrieves contacts for a specific page.
+     *
+     * @param page The page number.
+     * @return The list of contacts for the specified page.
+     */
     override suspend fun getContactsByPage(page: Int): List<Contact> {
         if (page == 1 || mapPaging.isEmpty()) {
             loadFromFile(paging = true)
@@ -37,12 +82,23 @@ class ContactsApiServiceImpl(
         return mapPaging[page].orEmpty()
     }
 
+    /**
+     * Retrieves all contacts as a single.
+     *
+     * @return The single containing the list of contacts.
+     */
     override fun getSingleContacts(): Single<List<Contact>> =
         Single.fromCallable {
             simulateError()
             loadFromFile(paging = false)
         }.delay(sleepTime, TimeUnit.MILLISECONDS)
 
+    /**
+     * Retrieves contacts for a specific page as a single.
+     *
+     * @param page The page number.
+     * @return The single containing the list of contacts for the specified page.
+     */
     override fun getSingleContactsByPage(page: Int): Single<List<Contact>> =
         Single.fromCallable {
             if (page == 1 || mapPaging.isEmpty()) {
@@ -51,8 +107,6 @@ class ContactsApiServiceImpl(
             simulateError()
             mapPaging[page].orEmpty()
         }.delay(sleepTime, TimeUnit.MILLISECONDS)
-
-    private val mapPaging: MutableMap<Int, List<Contact>> = hashMapOf()
 
     private fun loadFromFile(paging: Boolean = false): List<Contact> =
         resources.openRawResource(R.raw.contacts).bufferedReader().use { reader ->
