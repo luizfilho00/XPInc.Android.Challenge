0b4f6afaf77c6a094f096d22cbc5446f52530abc
Luiz Filho <luiz.filho@xpi.com.br>
2023-06-20 16:41:22 -0400

    Move to inner class

--- contacts/src/main/java/br/com/xpinc/mobile/contacts/adapter/ContactsAdapter.kt
+++ contacts/src/main/java/br/com/xpinc/mobile/contacts/adapter/ContactsAdapter.kt
@@ -12,12 +12,14 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 
-class ContactsAdapter : RecyclerView.Adapter<ContactsViewHolder>() {
+class ContactsAdapter : RecyclerView.Adapter<ContactsAdapter.ContactsViewHolder>() {
     private val items = mutableListOf<Contact>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactsViewHolder {
         return ContactsViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.contacts_view_holder, parent, false)
+            LayoutInflater
+                .from(parent.context)
+                .inflate(R.layout.contacts_view_holder, parent, false)
         )
     }
 
@@ -26,24 +28,23 @@ class ContactsAdapter : RecyclerView.Adapter<ContactsViewHolder>() {
     override fun onBindViewHolder(holder: ContactsViewHolder, position: Int) {
         holder.bind(items[position])
     }
-}
 
-class ContactsViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+    inner class ContactsViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        @SuppressLint("SetTextI18n")
+        fun bind(contact: Contact) {
+            loadAvatar(contact.email)
+            view.findViewById<TextView>(R.id.textViewName).text = "Nome: ${contact.name}"
+            view.findViewById<TextView>(R.id.textViewEmail).text = "Email: ${contact.email}"
+        }
 
-    @SuppressLint("SetTextI18n")
-    fun bind(contact: Contact) {
-        loadAvatar(contact.email)
-        view.findViewById<TextView>(R.id.textViewName).text = "Nome: ${contact.name}"
-        view.findViewById<TextView>(R.id.textViewEmail).text = "Email: ${contact.email}"
-    }
-
-    private fun loadAvatar(email: String) {
-        Glide.with(view)
-            .load("https://api.multiavatar.com/$email.png?apikey=YUWQMZ3LuEqFX5")
-            .transition(DrawableTransitionOptions.withCrossFade())
-            .diskCacheStrategy(DiskCacheStrategy.ALL)
-            .placeholder(R.drawable.avatar_placeholder)
-            .circleCrop()
-            .into(view.findViewById(R.id.imageViewAvatar))
+        private fun loadAvatar(email: String) {
+            Glide.with(view)
+                .load("https://api.multiavatar.com/$email.png?apikey=YUWQMZ3LuEqFX5")
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .placeholder(R.drawable.avatar_placeholder)
+                .circleCrop()
+                .into(view.findViewById(R.id.imageViewAvatar))
+        }
     }
 }
